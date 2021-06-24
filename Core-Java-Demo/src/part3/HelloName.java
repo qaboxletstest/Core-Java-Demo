d7a74bc0f350680fe0ac7c698a140f5b230fc56f
qaboxletstest <qaboxletstest@gmail.com>
2021-06-24 19:16:40 +0530

    Part-3

---
+++ Core-Java-Demo/src/part3/HelloName.java
@@ -0,0 +1,11 @@
+package part3;
+
+public class HelloName {
+
+	public static void main(String[] args) {
+			System.out.println("Hello " + args[0]);
+			int num = 10;
+			System.out.println("Number is "+ num);
+	}
+
+}
---
+++ Core-Java-Demo/src/part3/HelloWorld.java
@@ -0,0 +1,10 @@
+package part3;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println("Number is "+ num);
+	}
+
+}
