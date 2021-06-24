bea7b47f38168b5a0a29f577cd1962bc2b3249d6
qaboxletstest <qaboxletstest@gmail.com>
2021-06-24 19:24:07 +0530

    Part-6

--- Core-Java-Demo/src/TypeCasting.java
+++ Core-Java-Demo/src/TypeCasting.java
@@ -6,16 +6,20 @@ public class TypeCasting {
 		
 		// Widening Casting - Implicit
 		int numInt = 10;
+		long numLong = numInt;
 		double numDouble = numInt;
 		
 		System.out.println("myInt is "+ numInt);
+		System.out.println("numLong is "+ numLong);
 		System.out.println("numDouble is "+ numDouble);
 		
 		// Narrowing Casting - Implicit
 		double myDouble = 10.10d;
+		long myLong = (long)myDouble;
 		int myInt = (int)myDouble; // Manual casting - double to int
 		
 		System.out.println("myDouble is "+ myDouble);
+		System.out.println("myLong is "+ myLong);
 		System.out.println("myInt is "+ myInt);
 		
 	}
