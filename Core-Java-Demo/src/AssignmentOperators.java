4c7228ee0ea2bea9b1e8ec41a7b34dac3f823c8f
qaboxletstest <qaboxletstest@gmail.com>
2021-06-29 15:32:20 +0530

    Part-8

---
+++ Core-Java-Demo/src/AssignmentOperators.java
@@ -0,0 +1,35 @@
+
+public class TypeCasting {
+	
+
+	public static void main(String[] args)  {
+		
+		// Widening Casting - Implicit
+		int numInt = 10;
+		long numLong = numInt;
+		double numDouble = numInt;
+		
+		System.out.println("myInt is "+ numInt);
+		System.out.println("numLong is "+ numLong);
+		System.out.println("numDouble is "+ numDouble);
+		
+		// Narrowing Casting - Implicit
+		double myDouble = 10.10d;
+		long myLong = (long)myDouble;
+		int myInt = (int)myDouble; // Manual casting - double to int
+		
+		System.out.println("myDouble is "+ myDouble);
+		System.out.println("myLong is "+ myLong);
+		System.out.println("myInt is "+ myInt);
+		
+		long newNum = TypeCasting.addFive(234.567896);
+		System.out.println("Adding 5 returns "+ newNum );
+		
+	}
+	
+	
+	public static long addFive(double num) {
+		return (long)num + 5;
+	}
+
+}
